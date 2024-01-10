bd96369449201f3bcd8956e3c17c322c4654c7d6
SWEsnowman <100440384+SWEsnowman@users.noreply.github.com>
2024-01-10 17:27:25 -0600

    updated to correct use of fullcalendar

--- public/js/populate_calendar.js
+++ public/js/populate_calendar.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    axios.get()
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
         initialView: 'dayGridMonth',
-        displayEventEnd: true,
+        displayEventEnd: false,
+        // defaultTimedEventDuration: '01:00',
+        forceEventDuration: true,
         events: [
             { title: 'Meeting', start: new Date() }
         ],
@@ -20,14 +21,28 @@ document.addEventListener('DOMContentLoaded', function() {
             timeGridDay: { buttonText: 'day' }
           },
           eventClick: function(info) {
-            alert('Event: ' + info.event.title);
-            alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
-            alert('View: ' + info.view.type);
+            alert('Event: ' + info.event.title + '\n' + 'Start: ' + info.event.start + '\n' + 'End: ' + info.event.end + '\n');
         
             // change the border color just for fun
             info.el.style.borderColor = 'red';
           }
     });
+    var items = document.getElementsByClassName("testTime");
+    for(var i = 0; i<items.length;i++){
+        var item = items[i].textContent;
+        console.log(item);
+        addEvent("test", item, calendar);
+    }
     calendar.render();
+    console.log(calendar.getEvents());
   });
 
+
+  function addEvent(name, startTime, calendar) {
+    var eventData = {
+      title: name,
+      start: startTime,
+      allDay: false
+    };
+    calendar.addEvent(eventData);
+}
