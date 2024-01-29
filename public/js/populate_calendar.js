e3f70c419019005a5da3ae6587a8cb21ea2fe447
SWEsnowman <100440384+SWEsnowman@users.noreply.github.com>
2024-01-29 10:25:22 -0500

    whoops

--- public/js/populate_calendar.js
+++ public/js/populate_calendar.js
@@ -1,4 +1,4 @@
-const addEvent = new Event('addEvent');
+//const addEvent = new Event('addEvent');
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
