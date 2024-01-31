65381f135aa974f85f63f12ecc422562fc5e52c0
jprasad2 <147555000+jprasad2@users.noreply.github.com>
2024-01-31 14:28:40 -0500

    Update seed.js

--- tasks/seed.js
+++ tasks/seed.js
@@ -1,5 +1,5 @@
 import { closeConnection, dbConnection } from '../config/mongoConnections.js';
-import userData from "../data/instructors.js";
+import userData from "../data/users.js";
 
 (async () => {
   try {
@@ -9,8 +9,8 @@ import userData from "../data/instructors.js";
     console.log(e);
   }
   try{
-    await userData.createInstructor("Michael", "Bearint", "mbearint@stevens.edu","6096823797" ,"pass123", "instructor");
-    await userData.createInstructor("Josh", "Prasad", "jprasad2@stevens.edu","6034935270" ,"password123", "student");
+    await userData.createUser("Michael", "Bearint", "mbearint@stevens.edu","6096823797" ,"pass123", "instructor");
+    await userData.createUser("Josh", "Prasad", "jprasad2@stevens.edu","6034935270" ,"password123", "student");
   }catch(e){
     console.log(e)
   }
