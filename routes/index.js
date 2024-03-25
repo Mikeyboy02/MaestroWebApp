a2bbb333fd03a131dbef7cc2b3fdc8b611d80e14
kpate116 <kpate116@stevens.edu>
2024-03-25 14:30:31 -0400

    added comments to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
+//import routes for the different pages
 import instructorRoutes from './instructor_routes.js'
 import studentRoutes from './student_routes.js'
 import adminRoutes from './admin_routes.js'
 import generalRoutes from './general_routes.js'
 
+//set up routes for different pages
 const constructorMethod = (app) => {
     app.use('/', generalRoutes)
     app.use('/instructors', instructorRoutes)
